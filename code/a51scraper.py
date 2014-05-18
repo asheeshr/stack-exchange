811291a9ba89de92d674e1a4070a0c89089fb4ed
Asheesh <ashrj@outlook.com>
2014-05-18 20:01:13 +0530

    Added Site ID field in code and CSV

--- code/a51scraper.py
+++ code/a51scraper.py
@@ -19,39 +19,42 @@ SoupBeta = BeautifulSoup(PageBeta)
 #Process Page and Generate Output
 SiteList = SoupBeta.find_all("div", attrs={'class':'proposal-summary narrow'})
 for i in SiteList : 
-    temp_status = Status[i.contents[1].div.attrs['class'][1]]
-    name = i.contents[3].a.string
-    CompleteList.append((name, temp_status)) #Site Name, Status
-    print(name, temp_status)
+    site_id = i.attrs['id'][17:]
+    site_status = Status[i.contents[1].div.attrs['class'][1]]
+    site_name = i.contents[3].a.string
+    CompleteList.append((site_id, site_name, site_status)) #Site Id, Site Name, Status
+    print(site_id, site_name, site_status)
 
 #Go to next page
-
+#'''
 nextPage = SoupBeta.find_all("span", attrs={'class':'page-numbers next'})
 
 while nextPage : #Iterate till pages remain
-    time.sleep(1)#Throttle requests
+    time.sleep(2)#Throttle requests
     PageBeta = urllib.request.urlopen( baseURL + nextPage[0].parent.attrs['href'] + "&pagesize=50")
     SoupBeta = BeautifulSoup(PageBeta)
 
     #Process Page and Generate Output
     SiteList = SoupBeta.find_all("div", attrs={'class':'proposal-summary narrow'})
     for i in SiteList : 
-        temp_status = Status[i.contents[1].div.attrs['class'][1]]
+        site_id = i.attrs['id'][17:]
+        site_status = Status[i.contents[1].div.attrs['class'][1]]
 
-        if temp_status == 'Commitment' : continue #Skip proposals in commitment
+        if site_status == 'Commitment' : continue #Skip proposals in commitment
 
-        name = i.contents[3].a.string
-        CompleteList.append((name, temp_status)) #Site Name, Status
-        print(name, temp_status)
+        site_name = i.contents[3].a.string
+        CompleteList.append((site_id, site_name, site_status)) #Site Name, Status
+        print(site_id, site_name, temp_status)
 
     nextPage = SoupBeta.find_all("span", attrs={'class':'page-numbers next'})
-
+#'''
 
 print("Scraping complete!")
 
+
 CompleteListFile = open("../data/sitelist.csv", 'w')
-CompleteListFile.write("Site, Status" + '\n')
+CompleteListFile.write("Id, Site, Status" + '\n')
 for i in CompleteList :
-    CompleteListFile.write('"' + i[0][1:-1] + '"' + ', ' + i[1] + '\n')
+    CompleteListFile.write(i[0] + ', ' + '"' + i[1][1:-1] + '"' + ', ' + i[2] + '\n')
 
 CompleteListFile.close()
